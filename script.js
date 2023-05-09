4cee3f852d5a431626e884248f9060f6dfb09c56
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-05-09 18:29:27 -0400

    declare variable

---
+++ script.js
@@ -0,0 +1,4 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');

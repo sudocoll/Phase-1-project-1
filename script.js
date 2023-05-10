b6a69d17c492f1e556362e9b88552b24368f4e22
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-05-09 20:59:03 -0400

    Add function to retrieve meal recipe using fetch API

--- script.js
+++ script.js
@@ -38,3 +38,13 @@ function getMealList(){
         mealList.innerHTML = html;
     });
 }
+// get recipe of the meal
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
+}
